a419bf96ea79d992b8e14debf6662491e31a0175
Xabi <ale23@hotmail.fr>
2020-11-03 13:10:59 +0100

    update

--- PetStore/PetStore_database.sql
+++ PetStore/PetStore_database.sql
@@ -1,103 +1,103 @@
 CREATE TABLE CatalogFacade (
 	_id: VARCHAR(255),
-	emf: VARCJAR(255),
-	utx: VARCJAR(255),
-	bDebug: VARCJAR(255)
+	emf: VARCHAR(255),
+	utx: VARCHAR(255),
+	bDebug: VARCHAR(255)
 );
 		
 CREATE TABLE ZipLocation (
 	_id: VARCHAR(255),
-	zipCode: VARCJAR(255),
-	city: VARCJAR(255),
-	state: VARCJAR(255)
+	zipCode: VARCHAR(255),
+	city: VARCHAR(255),
+	state: VARCHAR(255)
 );
 		
 CREATE TABLE Address (
 	_id: VARCHAR(255),
-	addressID: VARCJAR(255),
-	street1: VARCJAR(255),
-	street2: VARCJAR(255),
-	city: VARCJAR(255),
-	state: VARCJAR(255),
-	zip: VARCJAR(255),
-	latitude: VARCJAR(255),
-	longitude: VARCJAR(255),
-	COMMA: VARCJAR(255)
+	addressID: VARCHAR(255),
+	street1: VARCHAR(255),
+	street2: VARCHAR(255),
+	city: VARCHAR(255),
+	state: VARCHAR(255),
+	zip: VARCHAR(255),
+	latitude: VARCHAR(255),
+	longitude: VARCHAR(255),
+	COMMA: VARCHAR(255)
 );
 		
 CREATE TABLE Category (
 	_id: VARCHAR(255),
-	categoryID: VARCJAR(255),
-	name: VARCJAR(255),
-	description: VARCJAR(255),
-	imageURL: VARCJAR(255)
+	categoryID: VARCHAR(255),
+	name: VARCHAR(255),
+	description: VARCHAR(255),
+	imageURL: VARCHAR(255)
 );
 		
 CREATE TABLE Product (
 	_id: VARCHAR(255),
-	productID: VARCJAR(255),
-	categoryID: VARCJAR(255),
-	name: VARCJAR(255),
-	description: VARCJAR(255),
-	imageURL: VARCJAR(255)
+	productID: VARCHAR(255),
+	categoryID: VARCHAR(255),
+	name: VARCHAR(255),
+	description: VARCHAR(255),
+	imageURL: VARCHAR(255)
 );
 		
 CREATE TABLE FileUploadResponse (
 	_id: VARCHAR(255),
-	itemId: VARCJAR(255),
-	productId: VARCJAR(255),
-	message: VARCJAR(255),
-	status: VARCJAR(255),
-	duration: VARCJAR(255),
-	durationString: VARCJAR(255),
-	startDate: VARCJAR(255),
-	endDate: VARCJAR(255),
-	uploadSize: VARCJAR(255),
-	thumbnail: VARCJAR(255)
+	itemId: VARCHAR(255),
+	productId: VARCHAR(255),
+	message: VARCHAR(255),
+	status: VARCHAR(255),
+	duration: VARCHAR(255),
+	durationString: VARCHAR(255),
+	startDate: VARCHAR(255),
+	endDate: VARCHAR(255),
+	uploadSize: VARCHAR(255),
+	thumbnail: VARCHAR(255)
 );
 		
 CREATE TABLE Item (
 	_id: VARCHAR(255),
-	itemID: VARCJAR(255),
-	productID: VARCJAR(255),
-	name: VARCJAR(255),
-	description: VARCJAR(255),
-	imageURL: VARCJAR(255),
-	imageThumbURL: VARCJAR(255),
-	price: VARCJAR(255),
-	address: VARCJAR(255),
-	contactInfo: VARCJAR(255),
-	totalScore: VARCJAR(255),
-	numberOfVotes: VARCJAR(255),
-	disabled: VARCJAR(255),
-	tags: VARCJAR(255)
+	itemID: VARCHAR(255),
+	productID: VARCHAR(255),
+	name: VARCHAR(255),
+	description: VARCHAR(255),
+	imageURL: VARCHAR(255),
+	imageThumbURL: VARCHAR(255),
+	price: VARCHAR(255),
+	address: VARCHAR(255),
+	contactInfo: VARCHAR(255),
+	totalScore: VARCHAR(255),
+	numberOfVotes: VARCHAR(255),
+	disabled: VARCHAR(255),
+	tags: VARCHAR(255)
 );
 		
 CREATE TABLE Tag (
 	_id: VARCHAR(255),
-	tagID: VARCJAR(255),
-	items: VARCJAR(255),
-	tag: VARCJAR(255),
-	refCount: VARCJAR(255)
+	tagID: VARCHAR(255),
+	items: VARCHAR(255),
+	tag: VARCHAR(255),
+	refCount: VARCHAR(255)
 );
 		
 CREATE TABLE SellerContactInfo (
 	_id: VARCHAR(255),
-	contactInfoID: VARCJAR(255),
-	lastName: VARCJAR(255),
-	firstName: VARCJAR(255),
-	email: VARCJAR(255)
+	contactInfoID: VARCHAR(255),
+	lastName: VARCHAR(255),
+	firstName: VARCHAR(255),
+	email: VARCHAR(255)
 );
 		
 CREATE TABLE RatingBean (
 	_id: VARCHAR(255),
-	itemId: VARCJAR(255),
-	grade: VARCJAR(255),
-	cf: VARCJAR(255)
+	itemId: VARCHAR(255),
+	grade: VARCHAR(255),
+	cf: VARCHAR(255)
 );
 		
 CREATE TABLE PayPalBean (
 	_id: VARCHAR(255),
-	postData: VARCJAR(255)
+	postData: VARCHAR(255)
 );
 		
--- tp.fil/src/tp/fil/main/DataComputation.java
+++ tp.fil/src/tp/fil/main/DataComputation.java
@@ -1,5 +1,6 @@
 package tp.fil.main;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.eclipse.emf.common.util.TreeIterator;
@@ -7,6 +8,8 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -14,17 +17,18 @@ import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.modisco.java.emf.JavaPackage;
 import org.eclipse.modisco.java.Model;
-//import org.eclipse.modisco.java.Package;
-//import org.eclipse.modisco.java.AbstractTypeDeclaration;
-//import org.eclipse.modisco.java.BodyDeclaration;
-//import org.eclipse.modisco.java.ClassDeclaration;
-//import org.eclipse.modisco.java.FieldDeclaration;
+import org.eclipse.modisco.java.Package;
+import org.eclipse.modisco.java.AbstractTypeDeclaration;
+import org.eclipse.modisco.java.BodyDeclaration;
+import org.eclipse.modisco.java.ClassDeclaration;
+import org.eclipse.modisco.java.FieldDeclaration;
 
 
 public class DataComputation {
 	
 	public static void main(String[] args) {
 		try {
+			System.out.println("Start ...");
 			Resource javaModel;
 			Resource dataModel;
 			Resource dataMetamodel;
@@ -47,11 +51,11 @@ public class DataComputation {
 				put("http://data", dataMetamodel.getContents().get(0));
 			
 			//Load Java model
-			javaModel = resSet.createResource(URI.createFileURI("../PetStore/PetStore_java.xmi"));
+			javaModel = resSet.createResource(URI.createFileURI("G:\\eclipse-modeling-workspace\\PetStore\\PetStore_java.xmi"));
 			javaModel.load(null);
 			
 			//Initiate Data model with a "Model" root object/element
-			dataModel = resSet.createResource(URI.createFileURI("../PetStore/PetStore_data.xmi"));
+			dataModel = resSet.createResource(URI.createFileURI("G:\\eclipse-modeling-workspace\\PetStore\\PetStore_data2.xmi"));
 			
 			EPackage dataPackage = (EPackage) dataMetamodel.getContents().get(0);
 			EClass modelClass = (EClass) dataPackage.getEClassifier("Model");
@@ -59,7 +63,6 @@ public class DataComputation {
 			
 			Model javaModelRootElement = (Model) javaModel.getContents().get(0);
 			modelObject.eSet(modelClass.getEStructuralFeature("name"), javaModelRootElement.getName());
-			
 			dataModel.getContents().add(modelObject);
 			
 			//Build the actual Data model by navigating the Java model
@@ -70,7 +73,36 @@ public class DataComputation {
 			 * Beginning of the part to be completed...
 			 */
 			
-			//TO BE COMPLETED
+			while(iterator.hasNext()) {
+				EObject currentModelElement = iterator.next();
+				if( currentModelElement.eClass().getName().equals("Model") ) {					
+					for( EObject e : currentModelElement.eContents()) {
+						if (e.eClass().getName().equals("Package")) {
+							for( EObject pc : e.eContents()) {
+								EClass packageClass = (EClass) dataPackage.getEClassifier("Package");
+								EObject packageObject = dataPackage.getEFactoryInstance().create(packageClass);
+								packageObject.eSet(packageClass.getEStructuralFeature("name"), pc.eGet(pc.eClass().getEStructuralFeature("name")));
+								dataPackage.getEClassifiers().add(packageObject.eClass());
+								//EObject totalWrapperObject = pc.eClass().getEFactoryInstance().create(totalWrapperClass);
+								//totalWrapperObject.eSet(totalWrapperClass.getEStructuralFeature("total"), totalSalary);							}
+							}
+						}
+					}
+					
+					EStructuralFeature attr = currentModelElement.eClass().getEStructuralFeature("name");
+					/*
+					 * TreeIterator<EObject> iteratorP = currentModelElement.eAllContents();
+					 * while(iteratorP.hasNext()) { EObject currentModelElementP = iterator.next();
+					 * }
+					 */
+					String name = (String) currentModelElement.eGet(attr);
+					currentModelElement.eSet(attr, name);
+				}
+			}
+			
+			dataModel.getContents().add(dataPackage);
+			
+			
 			
 			/*
 			 * End of the part to be completed...
@@ -82,7 +114,7 @@ public class DataComputation {
 			//Unload models
 			javaModel.unload();
 			dataModel.unload();
-			
+			System.out.println("Done !");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
